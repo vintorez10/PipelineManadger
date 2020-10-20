3a073fe933b33a06fbbe3ea25bbfb4c93eaafd1d
vintorez10 <vintorez10@mail.ru>
2020-10-20 12:34:41 +0300

    Update manualcontrolwindow.h

--- PipelineManagment/ManualControlWindow/manualcontrolwindow.h
+++ PipelineManagment/ManualControlWindow/manualcontrolwindow.h
@@ -1,18 +1,28 @@
 #ifndef MANUALCONTROLWINDOW_H
 #define MANUALCONTROLWINDOW_H
 
-#include <pipelinemanager.h>
-
-class PipelineManager;
+#include <QWidget>
+#include <QDebug>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QGroupBox>
+#include <QTableWidget>
+#include <QTableWidgetItem>
+#include <QTextBrowser>
+#include <QPalette>
+#include <QColor>
+#include <QLocale>
+#include <QSerialPort>
+#include <QSerialPortInfo>
+#include <QDateTime>
+#include <QString>
 
 class ManualControlWindow : public QWidget
 {
     Q_OBJECT
 
 public:
-    explicit ManualControlWindow(QWidget *parent = nullptr);
-
-    void initArduiniPort();    
+    explicit ManualControlWindow(QWidget *parent = nullptr);     
 
 private:
     enum{FirstButton = 0, SecondButton = 1, ThirdButton = 2,
@@ -25,19 +35,19 @@ private:
     QGroupBox* m_horisontalGroupBox;
     QVBoxLayout* m_mainLayout;
     QSerialPort* m_arduinoPort;
-    QString m_arduinoPortName;
-    PipelineManager* m_manager;
+    QString m_event, m_dataTime;
 
 public slots:
     void on_pushButtonFirstClicked();
     void on_pushButtonSecondClicked();
     void on_pushButtonThirdClicked();
     void on_pushButtonStopClicked();
-
+    void showWindow();
 
 signals:
-    void callMainWindow();
-    void sendData(QString dataTime, QString message);
+    void callMainWindow();    
+    void sendData(const QString& dataTime, const QString& message);
+    void sendCommand(const char* command);
 
 };
 
